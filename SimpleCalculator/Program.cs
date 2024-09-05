633c6a92fcd3645e36aea6e5f8ef8018d8b3461d
joseph_laptop <gregariousgamer0@gmail.com>
2024-09-05 13:20:21 -0400

    Serilog works

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace SimpleCalculator
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace SimpleCalculator
 {
     internal class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration() // Serilog needs connection to config file if settings stored there
+                .ReadFrom.Configuration(builder.Build())
+                .CreateLogger();
+
+            Log.Logger.Information("Application is starting, and hello!");
         }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+                .Build();
+        }
+
+
     }
 }
--- SimpleCalculator/obj/Debug/net8.0/SimpleCalculator.AssemblyInfo.cs
+++ SimpleCalculator/obj/Debug/net8.0/SimpleCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06badf7676d8652f1dee0f9d7a9a4c33ae43df6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cde9e09d3120004552a50a12dc29c064dc4a309")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
